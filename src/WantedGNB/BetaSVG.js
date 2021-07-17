2d320c0fd932bdd02c42638270b93e930a79e732
Park <pjane123@naver.com>
2021-07-17 19:09:23 +0900

    Update BetaSVG.js

--- src/WantedGNB/BetaSVG.js
+++ src/WantedGNB/BetaSVG.js
@@ -4,7 +4,7 @@ const BetaSVG = () => {
   console.log('beta');
   return (
     <svg xmlns='http://www.w3.org/2000/svg' width='18' height='11' viewBox='0 0 18 11'>
-      <g fill='none' fill-rule='evenodd' font-family='AppleSDGothicNeo-SemiBold, Apple SD Gothic Neo' font-size='9' font-weight='500'>
+      <g fill='none' fillRule='evenodd' fontFamily='AppleSDGothicNeo-SemiBold, Apple SD Gothic Neo' fontSize='9' fontWeight='500'>
         <g fill='#36F'>
           <g>
             <g>
