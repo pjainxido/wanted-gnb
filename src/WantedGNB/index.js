7f248db35b3bb7c54ac33226bfe373a62548afac
Park <pjane123@naver.com>
2021-07-17 19:09:38 +0900

    Update index.js

--- src/WantedGNB/index.js
+++ src/WantedGNB/index.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import './WantedGNB.scss';
 import Contents from './Contents';
+import SearchPopUp from './Contents/SearchPopUp';
 
-const Context = React.createContext();
+const GNBContext= createContext(null);
 
 function WantedGNB() {
   const [isLogIn, setLogIn] = useState(false);
-  const store = { isLogIn, setLogIn };
-  // const isDesktopOrMobile = useMediaQuery({ maxWidth: 767 });
+  const [isUserMenuPopup, setIsUserMenuPopup] = useState(false);
+  const [isNoticePopup, setIsNoticePopup] = useState(false);
+  const [isSearchPopup, setIsSearchPopup] = useState(false);
+  const store = { isLogIn,  setLogIn, isUserMenuPopup, setIsUserMenuPopup, isNoticePopup, setIsNoticePopup, isSearchPopup, setIsSearchPopup };
 
   return (
-    <Context.Provider value={store}>
-      <div className='gnb'>
+    <GNBContext.Provider value={store}>
+      <div className='wanted_gnb' role="presentation">
         <Contents/>
+        {isSearchPopup && <SearchPopUp/>}
       </div>
-    </Context.Provider>
+    </GNBContext.Provider>
   );
 }
 
 export default WantedGNB;
+
+export {GNBContext};
