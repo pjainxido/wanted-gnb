ece254bc708c54238274a5509898fe2e6e7d0451
Park <pjane123@naver.com>
2021-07-17 19:11:30 +0900

    Add SearchPopup components

---
+++ src/WantedGNB/Contents/SearchPopUp.js
@@ -0,0 +1,85 @@
+import React, {useContext}from 'react';
+import './SearchPopUp.scss';
+import { GNBContext } from '..';
+
+const SearchPopUp = () => {
+  const store = useContext(GNBContext);
+  const closeSearchPopUp = () => {
+    console.log('close!');
+    store.setIsSearchPopup(false);
+  }
+  return (
+    <div className='search_popup'>
+      <div className='search_container'>
+        <form role='presentation' action='.'>
+          <input type='search' placeholder='#태그, 회사, 포지션 검색' autocomplete='off' >
+          </input>
+          <svg className='searchIcon' xmlns='https://www.w3.org/2000/svg' xmlnsXlink='https://www.w3.org/1999/xlink' width='18' height='18' viewBox='0 0 18 18'>
+            <defs>
+              <path
+                id='qt2dnsql4a'
+                d='M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z'
+              ></path>
+            </defs>
+            <g fill='none' fillRule='evenodd'>
+              <use fill='#333' fillRule='nonzero' stroke='#333' strokeWidth='.3' xlinkHref='#qt2dnsql4a'></use>
+            </g>
+          </svg>
+          <button className='exitButton' type='button' onClick={closeSearchPopUp}>
+            <svg width='24' height='24' >
+              <path
+                fill='currentColor'
+                d='M17.97 19.03a.75.75 0 001.06-1.06l-6.5-6.5a.75.75 0 00-1.06 0l-6.5 6.5a.75.75 0 001.06 1.06L12 13.06l5.97 5.97zM12 10.94L6.03 4.97a.75.75 0 00-1.06 1.06l6.5 6.5a.75.75 0 001.06 0l6.5-6.5a.75.75 0 00-1.06-1.06L12 10.94z'
+              ></path>
+            </svg>
+          </button>
+        </form>
+        <div className='recommend' >
+          <div className='recommand_container'>
+            <h4 >추천태그로 검색해보세요</h4>
+            <a href='/tag_search' class='goCompanyTag' data-attribute-id='search__goCompanyTag'>
+              기업태그 홈 이동하기
+              <svg width='12' height='12' viewBox='0 0 12 12'>
+                <path
+                  fill='currentColor'
+                  d='M4.22 9.72a.75.75 0 001.06 1.06l4.25-4.25a.75.75 0 000-1.06L5.28 1.22a.75.75 0 00-1.06 1.06L7.94 6 4.22 9.72z'
+                ></path>
+              </svg>
+            </a>
+            <ul class='tagList'>
+              <li class='tagListItem'>
+                <button class='_3dAEjC_3V89KiWKlRYPTp_  undefined' type='button'>
+                  #차량지원
+                </button>
+              </li>
+              <li class='tagListItem'>
+                <button class='_3dAEjC_3V89KiWKlRYPTp_  undefined' type='button'>
+                  #퇴사율5%이하
+                </button>
+              </li>
+              <li class='tagListItem'>
+                <button class='_3dAEjC_3V89KiWKlRYPTp_  undefined' type='button'>
+                  #맥주
+                </button>
+              </li>
+              <li class='tagListItem'>
+                <button class='_3dAEjC_3V89KiWKlRYPTp_  undefined' type='button'>
+                  #유연근무
+                </button>
+              </li>
+              <li class='tagListItem'>
+                <button class='_3dAEjC_3V89KiWKlRYPTp_  undefined' type='button'>
+                  #연봉상위2~5%
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='dim' >
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPopUp;
--- src/WantedGNB/Contents/index.js
+++ src/WantedGNB/Contents/index.js
@@ -1,31 +1,35 @@
-import React, {useState} from 'react';
-import '../WantedGNB.scss';
+import React, { useState, useContext } from 'react';
+import { GNBContext } from '../';
 import BetaSVG from '../BetaSVG';
-import PopupMenu from '../PopupMenu';
+import MainPopUp from '../MainPopUp';
+import AsideMenu from '../AsideMenu';
+import '../WantedGNB.scss';
 
 const Contents = () => {
   const [isPopup, setIsPopup] = useState(false);
+  const store = useContext(GNBContext);
+
   return (
-    <div className='contents'>
+    <div className={store.isLogIn ? 'contents isLogIn' : 'contents'}>
       <nav className='gnb'>
         <div className='logo_signup_container'>
           <a href='/' className='logo'>
-            <i className='icon'>
-              logo
-              </i>
+            <i className='icon'>wanted</i>
           </a>
-          {/* <button className='signupLogin' type='button'>
-            회원가입하기
-          </button> */}
+          {!store.isLogIn && (
+            <button className='signup_login' type='button' onClick={()=>{store.setLogIn(true)}}>
+              회원가입하기
+            </button>
+          )}
         </div>
         <ul className='navBar'>
-          <li className='onMobileDisplay' data-attribute-id='gnb' data-gnb-kind='home'>
+          <li className='mobileHomeButton' data-attribute-id='gnb' data-gnb-kind='home'>
             <a href='/' className='home'>
               홈
             </a>
           </li>
-          <li onMouseEnter={()=>setIsPopup(true)} data-attribute-id='gnb' data-gnb-kind='explore'>
-            <a href='/wdlist' >탐색</a>
+          <li onMouseEnter={() => setIsPopup(true)} data-attribute-id='gnb' data-gnb-kind='explore'>
+            <a href='/wdlist'>탐색</a>
           </li>
           <li data-attribute-id='gnb' data-gnb-kind='event'>
             <a href='/events'>커리어 성장</a>
@@ -41,7 +45,7 @@ const Contents = () => {
           <li className='moreVisible' data-attribute-id='gnb' data-gnb-kind='matchup'>
             <a href='/matchup/intro/'>매치업</a>
           </li>
-          <li className='moreVisible'data-attribute-id='gnb' data-gnb-kind='freelancer'>
+          <li className='moreVisible' data-attribute-id='gnb' data-gnb-kind='freelancer'>
             <a href='https://www.wanted.co.kr/gigs/experts' target='_blank' rel='noopener noreferrer'>
               프리랜서
             </a>
@@ -55,57 +59,8 @@ const Contents = () => {
             </a>
           </li>
         </ul>
-        <PopupMenu isPopup={isPopup} onMouseLeave={()=>setIsPopup(false)}/>
-        <aside className='asideContainer'>
-          <ul>
-            <li>
-              <button class='searchButton' type='button' data-attribute-id='gnb' data-gnb-kind='search'>
-                <svg xmlns='https://www.w3.org/2000/svg' xmlnsXlink='https://www.w3.org/1999/xlink' width='18' height='18' viewBox='0 0 18 18'>
-                  <defs>
-                    <path
-                      id='qt2dnsql4a'
-                      d='M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z'
-                    ></path>
-                  </defs>
-                  <g fill='none' fill-rule='evenodd'>
-                    <use fill='#333' fill-rule='nonzero' stroke='#333' stroke-width='.3' xlinkHref='#qt2dnsql4a'></use>
-                  </g>
-                </svg>
-              </button>
-            </li>
-            <li>
-              <button class='signUpButton' type='button' data-attribute-id='gnb' data-gnb-kind='signupLogin'>
-                회원가입/로그인
-              </button>
-            </li>
-            <li class='smMoreVisible leftDivision' data-attribute-id='gnb' data-gnb-kind='forEmployers'>
-              <a class='dashboardButton' href='/dashboard'>
-                기업 서비스
-              </a>
-            </li>
-            <li class='_33WfrN55nCu3Vs3WKDuomx'>
-              <button className='menuButton' type='button' data-attribute-id='gnb' data-gnb-kind='photo'>
-                <svg xmlns='https://www.w3.org/2000/svg' xmlnsXlink='https://www.w3.org/1999/xlink' width='18' height='18' viewBox='0 0 18 18'>
-                  <defs>
-                    <path
-                      id='8srz5wswoa'
-                      d='M15 13.688c.31 0 .563.251.563.562 0 .31-.252.563-.563.563H3c-.31 0-.563-.252-.563-.563 0-.31.252-.563.563-.563zm0-5.25c.31 0 .563.251.563.562 0 .31-.252.563-.563.563H3c-.31 0-.563-.252-.563-.563 0-.31.252-.563.563-.563zm0-5.25c.31 0 .563.251.563.562 0 .31-.252.563-.563.563H3c-.31 0-.563-.252-.563-.563 0-.31.252-.563.563-.563z'
-                    ></path>
-                  </defs>
-                  <g fill='none' fill-rule='evenodd' transform='translate(-337 -19) translate(337 19)'>
-                    <mask id='28iwa9h2ub' fill='#fff'>
-                      <use xlinkHref='#8srz5wswoa'></use>
-                    </mask>
-                    <use fill='#333' fill-rule='nonzero' stroke='#333' stroke-width='.3' xlinkHref='#8srz5wswoa'></use>
-                    <g fill='#333' mask='url(#28iwa9h2ub)'>
-                      <path d='M0 0H18V18H0z'></path>
-                    </g>
-                  </g>
-                </svg>
-              </button>
-            </li>
-          </ul>
-        </aside>
+        <MainPopUp isPopup={isPopup} onMouseLeave={() => setIsPopup(false)} />
+        <AsideMenu />
       </nav>
     </div>
   );
